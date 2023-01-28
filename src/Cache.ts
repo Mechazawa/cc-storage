457f1df15fa9f148384a708c94d6add76d2f19a7
shodan <mega@ioexception.at>
2023-01-28 02:31:30 +0100

    Tweak cache class

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,38 +1,38 @@
 import Logger from "./Logger";
 import Serializable from "./Serializable";
 
-export default class Cache extends Serializable {
-  cache = new LuaMap<string, any>();
+export default class Cache<T = any> extends Serializable {
+  store = new LuaMap<string, T>();
 
-  set(name: string, value: any) {
-    this.cache.set(name, value);
+  set(name: string, value: T) {
+    this.store.set(name, value);
   }
 
   flush(): void {
-    this.cache = new LuaMap<string, any>();
+    this.store = new LuaMap<string, T>();
   }
 
   delete(name: string): number {
     if (name.endsWith("*")) {
       name = name.slice(0, name.length - 1);
 
-      const keys = Object.keys(this.cache).filter((key) => key.startsWith(name));
+      const keys = Object.keys(this.store).filter((key) => key.startsWith(name));
 
-      return keys.map((key) => this.cache.delete(key)).filter((x) => x).length;
+      return keys.map((key) => this.store.delete(key)).filter((x) => x).length;
     }
 
-    return this.cache.delete(name) ? 1 : 0;
+    return this.store.delete(name) ? 1 : 0;
   }
 
-  get(name: string): any | undefined {
-    return this.cache.get(name);
+  get(name: string): T | undefined {
+    return this.store.get(name);
   }
 
   has(name: string): boolean {
-    return true === this.cache.has(name);
+    return true === this.store.has(name);
   }
 
-  remember(name: string, fn: () => any): any {
+  remember(name: string, fn: () => T): T {
     let value = this.get(name);
 
     if (value) {
@@ -54,14 +54,14 @@ export default class Cache extends Serializable {
     }) as unknown as T;
   }
 
-  serialise(): LuaMap<string, any> {
-    return this.cache;
+  serialise(): LuaMap<string, T> {
+    return this.store;
   }
 
   static deserialize(input: LuaMap<string, any>): Cache {
     const instance = new this();
 
-    instance.cache = input;
+    instance.store = input;
 
     return instance;
   }
