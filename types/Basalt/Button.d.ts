00e002afaa3a035c918bc08904b888a589ee7a37
Mechazawa <mega@ioexception.at>
2023-02-08 22:55:40 +0100

    Remove junk doc from Basalt/Button

--- types/Basalt/Button.d.ts
+++ types/Basalt/Button.d.ts
@@ -21,7 +21,6 @@ declare interface Button extends BObject {
 
   /**
  * Sets the displayed button text
-1. `string` the text the button should show
  *
  * @param text text the button should show
  * @returns {this} The object in use
