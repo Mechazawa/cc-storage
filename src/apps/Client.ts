169947d59a9265d6cf157fe4aafac9c71bce776e
jellehamoen <jelle@jellehamoen.nl>
2023-02-26 15:37:37 +0100

    get test

--- src/apps/Client.ts
+++ src/apps/Client.ts
@@ -128,7 +128,8 @@ export default class Client extends App {
     if (searchResults) {
       // Check whether list exists
       searchResults.forEach((e) => {
-        itemList.addItem(`${e.count}  ${e.displayName}`, colors.black, colors.white, e.key);
+        // itemList.addItem(`${e.count}  ${e.displayName}`, colors.black, colors.white, e.key);
+        itemList.addItem(`${e.key}`, colors.black, colors.white);
       });
     } else {
       this.logger.error("item list not found");
@@ -200,7 +201,7 @@ export default class Client extends App {
       // this.logger.debug("Clicked store all button");
       // Implement function here to empty client into network
       takeButtonSingle.setBackground(colors.lightBlue);
-      const itemKey = itemList.getItem(itemList.getItemIndex()).get('value');
+      const itemKey = itemList.getItem(itemList.getItemIndex()).get('text');
       this.logger.debug(itemKey);
       this.server?.take(this.config.storage[0], itemKey, 1);
     });
